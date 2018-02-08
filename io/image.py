ddb010e62ba638108426c9e34ffc8411df50ed31
Jeff Markowitz <markowitzmeister@gmail.com>
2018-02-08 18:34:35 -0500

    Last minute cleanup for image functions

--- io/image.py
+++ io/image.py
@@ -11,15 +11,13 @@ def write_image(filename, image, scale=True, dtype='uint16', metadata={}):
     if scale:
         iinfo=np.iinfo(dtype)
         image=image.astype(dtype)
-
         scale_factor=np.floor(iinfo.max/image.max()).astype(dtype)
-
         scaled_image=image*scale_factor
         metadata={'scale_factor':str(scale_factor)}
 
     tifffile.imsave(filename, scaled_image, compress=0, metadata=metadata)
 
-def read_image(filename, dtype='uint16', scale_key=None):
+def read_image(filename, dtype='uint16', scale=True, scale_key='scale_factor'):
     """Load image data, possibly with scale factor...
     """
 
@@ -28,7 +26,7 @@ def read_image(filename, dtype='uint16', scale_key=None):
 
     image=tmp.asarray()
 
-    if scale_key:
+    if scale:
         image_desc=json.loads(tmp.pages[0].tags['image_description'].as_str()[2:-1])
         scale_factor=int(image_desc[scale_key])
         image=image/scale_factor
