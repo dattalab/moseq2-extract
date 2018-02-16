49f8878cda175ce35ccc1d5b464ea483eb433f17
jmarkow <jmarkow@cns.bu.edu>
2018-02-16 16:48:42 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.01a',
     platforms=['mac','unix'],
     install_requires=['h5py','matplotlib','scipy>=0.19','scikit-image==0.13','scikit-learn==0.19',
-                      'tqdm','statsmodels==0.8.0','numpy==1.13.1','joblib==0.11'],
-    python_requires='>=3',
+                      'tqdm','statsmodels==0.8.0','numpy==1.13.1','joblib==0.11','opencv-python','click'],
+    python_requires='>=3.5',
     entry_points={'console_scripts':['moseq2 = moseq2.cli:extract']}
 )
