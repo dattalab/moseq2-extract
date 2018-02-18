3d868e6cb542bb86b66bab71efa0a805c4aed586
Jeff Markowitz <markowitzmeister@gmail.com>
2018-02-18 17:07:40 -0500

    Added image tests

--- moseq2/io/image.py
+++ moseq2/io/image.py
@@ -2,6 +2,7 @@ from skimage.external import tifffile
 import numpy as np
 import json
 import os
+import ast
 
 def write_image(filename, image, scale=True, scale_factor=None, dtype='uint16', metadata={}):
     """Save image data, possibly with scale factor for easy display
@@ -27,9 +28,9 @@ def write_image(filename, image, scale=True, scale_factor=None, dtype='uint16', 
         metadata={'scale_factor':str(scale_factor)}
 
     directory=os.path.dirname(filename)
-    if not os.path.exists(directory):
+    if directory and not os.path.exists(directory):
         os.makedirs(directory)
-    
+
     tifffile.imsave(filename, image, compress=0, metadata=metadata)
 
 def read_image(filename, dtype='uint16', scale=True, scale_key='scale_factor'):
@@ -43,7 +44,17 @@ def read_image(filename, dtype='uint16', scale=True, scale_key='scale_factor'):
 
     if scale:
         image_desc=json.loads(tmp.pages[0].tags['image_description'].as_str()[2:-1])
-        scale_factor=int(image_desc[scale_key])
-        image=image/scale_factor
+
+        try:
+            scale_factor=int(image_desc[scale_key])
+        except:
+            scale_factor=ast.literal_eval(image_desc[scale_key])
+
+        if type(scale_factor) is int:
+            image=image/scale_factor
+        elif type(scale_factor) is tuple:
+            iinfo=np.iinfo(image.dtype)
+            image=image.astype('float32')/iinfo.max
+            image=image*(scale_factor[1]-scale_factor[0])+scale_factor[0]
 
     return image
---
+++ tests/test_io_image.py
@@ -0,0 +1,45 @@
+import numpy.testing as npt
+import numpy as np
+import pytest
+import os
+from moseq2.io.image import write_image,read_image
+from skimage.external import tifffile
+
+@pytest.fixture(scope='function')
+def image_file(tmpdir):
+    f=tmpdir.mkdir('test_dir')
+    return str(f)
+
+def test_write_image(image_file):
+
+    # make some random ints, don't exceed 16 bit limits
+
+    image_file=os.path.join(image_file,'test2','test_image.tiff')
+    rnd_img=np.random.randint(low=0,high=100,size=(50,50)).astype('uint16')
+    write_image(image_file,rnd_img,scale=False)
+
+    with tifffile.TiffFile(image_file) as tif:
+        tmp=tif
+
+    image=tmp.asarray().astype('uint16')
+    npt.assert_almost_equal(rnd_img,image,3)
+
+def test_read_image(image_file):
+
+    image_file=os.path.join(image_file,'test2','test_image.tiff')
+    rnd_img=np.random.randint(low=0,high=100,size=(50,50)).astype('uint16')
+
+    write_image(image_file,rnd_img,scale=True)
+    image=read_image(image_file,scale=True)
+
+    npt.assert_almost_equal(rnd_img,image,3)
+
+    write_image(image_file,rnd_img,scale=True,scale_factor=(0,100))
+    image=read_image(image_file,scale=True)
+
+    npt.assert_almost_equal(rnd_img,image,3)
+
+    write_image(image_file,rnd_img,scale=False)
+    image=read_image(image_file,scale=False)
+
+    npt.assert_almost_equal(rnd_img,image,3)
