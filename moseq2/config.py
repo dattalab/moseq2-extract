3a441e33af63401ff7403d480d2dd826390de07a
wingillis <win.gillis@gmail.com>
2018-04-11 14:14:46 -0400

    Adding file to handle configurations

---
+++ moseq2/config.py
@@ -0,0 +1,8 @@
+'''
+Author: @wingillis
+Module to handle operations dealing with moseq configurations.
+There are two ways to define moseq parameters:
+    - using the cli-based flags
+    - using a configuration file
+Any cli-based flags will override parameters defined in a config file
+'''
