d7ff2ad05f696f18437f0863fff6ecb1230c540a
jmarkow <jmarkow@cns.bu.edu>
2018-07-01 10:19:36 -0400

    Updated requirements to be a little more liberal for setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     version='0.02a',
     platforms=['mac', 'unix'],
     install_requires=['h5py', 'matplotlib', 'scipy>=0.19',
-                      'scikit-image==0.13', 'scikit-learn==0.19',
-                      'tqdm', 'statsmodels==0.8.0',
-                      'numpy==1.13.1', 'joblib==0.11', 'opencv-python',
+                      'scikit-image>=0.13', 'scikit-learn>=0.19',
+                      'tqdm', 'statsmodels>=0.8.0',
+                      'numpy', 'joblib==0.11', 'opencv-python',
                       'click', 'ruamel.yaml'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-extract = moseq2_extract.cli:cli']}
