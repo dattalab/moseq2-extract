ddfd6b9cfdf802ebbb221450638e6bebd99e6793
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-09-11 21:11:01 -0400

    Updated tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -63,6 +63,106 @@ def test_extract(temp_dir):
     assert(result.exit_code == 0)
 
 
+def test_extract_trim(temp_dir):
+
+    data_path = os.path.join(temp_dir, 'depth.dat')
+    edge_size = 40
+    points = np.arange(-edge_size, edge_size)
+    sig1 = 10
+    sig2 = 20
+
+    kernel = np.exp(-(points**2.0) / (2.0 * sig1**2.0))
+    kernel2 = np.exp(-(points**2.0) / (2.0 * sig2**2.0))
+
+    kernel_full = np.outer(kernel, kernel2)
+    kernel_full /= np.max(kernel_full)
+    kernel_full *= 50
+
+    fake_mouse = kernel_full
+    fake_mouse[fake_mouse < 5] = 0
+
+    tmp_image = np.ones((424, 512), dtype='int16') * 1000
+    center = np.array(tmp_image.shape) // 2
+
+    mouse_dims = np.array(fake_mouse.shape) // 2
+
+    # put a mouse on top of a disk
+
+    roi = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (300, 300)).astype('int16') * 300
+    roi_dims = np.array(roi.shape) // 2
+
+    tmp_image[center[0]-roi_dims[0]:center[0]+roi_dims[0],
+              center[1]-roi_dims[1]:center[1]+roi_dims[1]] =\
+        tmp_image[center[0]-roi_dims[0]:center[0]+roi_dims[0],
+                  center[1]-roi_dims[1]:center[1]+roi_dims[1]]-roi
+
+    tmp_image[center[0]-mouse_dims[0]:center[0]+mouse_dims[0],
+              center[1]-mouse_dims[1]:center[1]+mouse_dims[1]] =\
+        tmp_image[center[0]-mouse_dims[0]:center[0]+mouse_dims[0],
+                  center[1]-mouse_dims[1]:center[1]+mouse_dims[1]]-fake_mouse
+
+    fake_movie = np.tile(tmp_image, (300, 1, 1))
+    fake_movie.tofile(data_path)
+
+    runner = CliRunner()
+    result = runner.invoke(extract, [data_path,
+                                     '--output-dir', temp_dir,
+                                     '--frame-trim', 20, 50],
+                           catch_exceptions=True)
+    print(result.output)
+    assert(result.exit_code == 0)
+
+
+# def test_extract_em(temp_dir):
+#
+#     data_path = os.path.join(temp_dir, 'depth.dat')
+#     edge_size = 40
+#     points = np.arange(-edge_size, edge_size)
+#     sig1 = 10
+#     sig2 = 20
+#
+#     kernel = np.exp(-(points**2.0) / (2.0 * sig1**2.0))
+#     kernel2 = np.exp(-(points**2.0) / (2.0 * sig2**2.0))
+#
+#     kernel_full = np.outer(kernel, kernel2)
+#     kernel_full /= np.max(kernel_full)
+#     kernel_full *= 50
+#
+#     fake_mouse = kernel_full
+#     fake_mouse[fake_mouse < 5] = 0
+#
+#     tmp_image = np.ones((424, 512), dtype='int16') * 1000
+#     center = np.array(tmp_image.shape) // 2
+#
+#     mouse_dims = np.array(fake_mouse.shape) // 2
+#
+#     # put a mouse on top of a disk
+#
+#     roi = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (300, 300)).astype('int16') * 300
+#     roi_dims = np.array(roi.shape) // 2
+#
+#     tmp_image[center[0]-roi_dims[0]:center[0]+roi_dims[0],
+#               center[1]-roi_dims[1]:center[1]+roi_dims[1]] =\
+#         tmp_image[center[0]-roi_dims[0]:center[0]+roi_dims[0],
+#                   center[1]-roi_dims[1]:center[1]+roi_dims[1]]-roi
+#
+#     tmp_image[center[0]-mouse_dims[0]:center[0]+mouse_dims[0],
+#               center[1]-mouse_dims[1]:center[1]+mouse_dims[1]] =\
+#         tmp_image[center[0]-mouse_dims[0]:center[0]+mouse_dims[0],
+#                   center[1]-mouse_dims[1]:center[1]+mouse_dims[1]]-fake_mouse
+#
+#     fake_movie = np.tile(tmp_image, (300, 1, 1))
+#     fake_movie.tofile(data_path)
+#
+#     runner = CliRunner()
+#     result = runner.invoke(extract, [data_path,
+#                                      '--output-dir', temp_dir,
+#                                      '--use-tracking-model', True],
+#                            catch_exceptions=True)
+#     print(result.output)
+#     assert(result.exit_code == 0)
+
+
 def test_find_roi(temp_dir):
 
     data_path = os.path.join(temp_dir, 'depth.dat')
