4cf70be785c8040261247e741b37c91e6d89a7f5
Ayman Zeine <a.zeine.96@gmail.com>
2019-09-24 12:15:22 -0400

    Updated GUI functions to be compatible with notebook

--- moseq2_extract/gui.py
+++ moseq2_extract/gui.py
@@ -2,6 +2,9 @@ from .cli import *
 import ruamel.yaml as yaml
 from moseq2_extract.io.video import (get_movie_info, load_movie_data,
                                      write_frames_preview, write_frames)
+import urllib
+import h5py
+import tqdm
 from moseq2_extract.io.image import write_image, read_image
 from moseq2_extract.extract.extract import extract_chunk
 from moseq2_extract.extract.proc import apply_roi, get_roi, get_bground_im_file
@@ -10,7 +13,7 @@ from moseq2_extract.util import (load_metadata, gen_batch_sequence, load_timesta
                                  save_dict_contents_to_h5, click_param_annot,
                                  convert_raw_to_avi_function)
 
-def generate_config_command(output_file, gui_options):
+def generate_config_command(output_file, gui_options={}):
     if gui_options == {}:
         objs = extract.params
         params = {tmp.name: tmp.default for tmp in objs if not tmp.required}
@@ -22,6 +25,42 @@ def generate_config_command(output_file, gui_options):
         with open(output_file, 'w') as f:
             yaml.dump(params, f, Dumper=yaml.RoundTripDumper)
 
+def download_flip_command(output_dir, selected_flip):
+
+
+    # TODO: more flip files!!!!
+    flip_files = {
+        'large mice with fibers':
+            "https://storage.googleapis.com/flip-classifiers/flip_classifier_k2_largemicewithfiber.pkl",
+        'adult male c57s':
+            "https://storage.googleapis.com/flip-classifiers/flip_classifier_k2_c57_10to13weeks.pkl",
+        'mice with Inscopix cables':
+            "https://storage.googleapis.com/flip-classifiers/flip_classifier_k2_inscopix.pkl"
+    }
+
+    key_list = list(flip_files.keys())
+    for idx, (k, v) in enumerate(flip_files.items()):
+        print('[{}] {} ---> {}'.format(idx, k, v))
+
+    selection = None
+
+    if selected_flip is not None:
+        selection = selected_flip
+
+    while selection is None:
+        selection = click.prompt('Enter a selection', type=int)
+        if selection > len(flip_files.keys()):
+            selection = None
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    selection = flip_files[key_list[selection]]
+
+    output_filename = os.path.join(output_dir, os.path.basename(selection))
+    urllib.request.urlretrieve(selection, output_filename)
+    print('Successfully downloaded flip file to {}'.format(output_filename))
+    print('Be sure to supply this as your flip-file during extraction')
 
 def convert_raw_to_avi_command(input_file, output_file, chunk_size, fps, delete, threads):
 
@@ -171,15 +210,11 @@ def find_roi_command(input_file, bg_roi_dilate, bg_roi_shape, bg_roi_index, bg_r
     return True
 
 
-def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_index, bg_roi_weights, bg_roi_depth_range,
-            bg_roi_gradient_filter, bg_roi_gradient_threshold, bg_roi_gradient_kernel, bg_roi_fill_holes,
-            min_height, max_height, fps, flip_classifier, flip_classifier_smoothing,
-            use_tracking_model, tracking_model_ll_threshold, tracking_model_mask_threshold,
-            tracking_model_ll_clip, tracking_model_segment, tracking_model_init, cable_filter_iters, cable_filter_shape,
-            cable_filter_size, tail_filter_iters, tail_filter_size, tail_filter_shape, spatial_filter_size,
-            temporal_filter_size, chunk_size, chunk_overlap, output_dir, write_movie, use_plane_bground,
-            frame_dtype, centroid_hampel_span, centroid_hampel_sig, angle_hampel_span, angle_hampel_sig,
-            model_smoothing_clips, frame_trim, config_file, compress, compress_chunk_size, compress_threads):
+def extract_command(input_file, output_dir, config_file):
+
+    with open(config_file, 'r') as f:
+        config_data = yaml.safe_load(f)
+
 
     print('Processing: {}'.format(input_file))
     # get the basic metadata
@@ -217,13 +252,13 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
     #     # convert TarInfo into bufferedreader
     #     input_file = tar.extractfile(input_file)
 
-    if frame_trim[0] > 0 and frame_trim[0] < nframes:
-        first_frame_idx = frame_trim[0]
+    if config_data['frame_trim'][0] > 0 and config_data['frame_trim'][0] < nframes:
+        first_frame_idx = config_data['frame_trim'][0]
     else:
         first_frame_idx = 0
 
-    if nframes - frame_trim[1] > first_frame_idx:
-        last_frame_idx = nframes - frame_trim[1]
+    if nframes - config_data['frame_trim'][1] > first_frame_idx:
+        last_frame_idx = nframes - config_data['frame_trim'][1]
     else:
         last_frame_idx = nframes
 
@@ -258,7 +293,7 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
     scalars_attrs = scalar_attributes()
     scalars = list(scalars_attrs.keys())
 
-    frame_batches = list(gen_batch_sequence(nframes, chunk_size, chunk_overlap))
+    frame_batches = list(gen_batch_sequence(nframes, config_data['chunk_size'], config_data['chunk_overlap']))
 
     # set up the output directory
 
@@ -270,7 +305,7 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    output_filename = 'results_{:02d}'.format(bg_roi_index)
+    output_filename = 'results_{:02d}'.format(config_data['bg_roi_index'])
     status_filename = os.path.join(output_dir, '{}.yaml'.format(output_filename))
 
     if os.path.exists(status_filename):
@@ -287,18 +322,18 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
     else:
         print('Getting background...')
         bground_im = get_bground_im_file(input_file, tar_object=tar)
-        if not use_plane_bground:
+        if not config_data['use_plane_bground']:
             write_image(os.path.join(output_dir, 'bground.tiff'), bground_im, scale=True)
 
     first_frame = load_movie_data(input_file, 0, tar_object=tar)
     write_image(os.path.join(output_dir, 'first_frame.tiff'), first_frame, scale=True,
-                scale_factor=bg_roi_depth_range)
+                scale_factor=config_data['bg_roi_depth_range'])
 
-    roi_filename = 'roi_{:02d}.tiff'.format(bg_roi_index)
+    roi_filename = 'roi_{:02d}.tiff'.format(config_data['bg_roi_index'])
 
-    strel_dilate = select_strel(bg_roi_shape, bg_roi_dilate)
-    strel_tail = select_strel(tail_filter_shape, tail_filter_size)
-    strel_min = select_strel(cable_filter_shape, cable_filter_size)
+    strel_dilate = select_strel(config_data['bg_roi_shape'], config_data['bg_roi_dilate'])
+    strel_tail = select_strel(config_data['tail_filter_shape'], config_data['tail_filter_size'])
+    strel_min = select_strel(config_data['cable_filter_shape'], config_data['cable_filter_size'])
 
     if os.path.exists(os.path.join(output_dir, roi_filename)):
         print('Loading ROI...')
@@ -307,14 +342,14 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
         print('Getting roi...')
         rois, plane, _, _, _, _ = get_roi(bground_im,
                                           strel_dilate=strel_dilate,
-                                          weights=bg_roi_weights,
-                                          depth_range=bg_roi_depth_range,
-                                          gradient_filter=bg_roi_gradient_filter,
-                                          gradient_threshold=bg_roi_gradient_threshold,
-                                          gradient_kernel=bg_roi_gradient_kernel,
-                                          fill_holes=bg_roi_fill_holes)
+                                          weights=config_data['bg_roi_weights'],
+                                          depth_range=config_data['bg_roi_depth_range'],
+                                          gradient_filter=config_data['bg_roi_gradient_filter'],
+                                          gradient_threshold=config_data['bg_roi_gradient_threshold'],
+                                          gradient_kernel=config_data['bg_roi_gradient_kernel'],
+                                          fill_holes=config_data['bg_roi_fill_holes'])
 
-        if use_plane_bground:
+        if config_data['use_plane_bground']:
             print('Using plane fit for background...')
             xx, yy = np.meshgrid(np.arange(bground_im.shape[1]), np.arange(bground_im.shape[0]))
             coords = np.vstack((xx.ravel(), yy.ravel()))
@@ -323,7 +358,7 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
             write_image(os.path.join(output_dir, 'bground.tiff'), plane_im, scale=True)
             bground_im = plane_im
 
-        roi = rois[bg_roi_index]
+        roi = rois[config_data['bg_roi_index']]
         write_image(os.path.join(output_dir, roi_filename),
                     roi, scale=True, dtype='uint8')
 
@@ -342,17 +377,17 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
             f.create_dataset('timestamps', compression='gzip', data=timestamps)
             f['timestamps'].attrs['description'] = "Depth video timestamps"
 
-        f.create_dataset('frames', (nframes, crop_size[0], crop_size[1]), frame_dtype, compression='gzip')
+        f.create_dataset('frames', (nframes, config_data['crop_size'][0], config_data['crop_size'][1]), config_data['frame_dtype'], compression='gzip')
         f['frames'].attrs['description'] = '3D Numpy array of depth frames (nframes x w x h, in mm)'
 
-        if use_tracking_model:
-            f.create_dataset('frames_mask', (nframes, crop_size[0], crop_size[1]), 'float32', compression='gzip')
+        if config_data['use_tracking_model']:
+            f.create_dataset('frames_mask', (nframes, config_data['crop_size'][0], config_data['crop_size'][1]), 'float32', compression='gzip')
             f['frames_mask'].attrs['description'] = 'Log-likelihood values from the tracking model (nframes x w x h)'
         else:
-            f.create_dataset('frames_mask', (nframes, crop_size[0], crop_size[1]), 'bool', compression='gzip')
+            f.create_dataset('frames_mask', (nframes, config_data['crop_size'][0], config_data['crop_size'][1]), 'bool', compression='gzip')
             f['frames_mask'].attrs['description'] = 'Boolean mask, false=not mouse, true=mouse'
 
-        if flip_classifier is not None:
+        if config_data['flip_classifier'] is not None:
             f.create_dataset('metadata/extraction/flips', (nframes, ), 'bool', compression='gzip')
             f['metadata/extraction/flips'].attrs['description'] = 'Output from flip classifier, false=no flip, true=flip'
 
@@ -395,50 +430,50 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
             raw_frames = load_movie_data(input_file, [f + first_frame_idx for f in frame_range], tar_object=tar)
             raw_frames = bground_im-raw_frames
             # raw_frames[np.logical_or(raw_frames < min_height, raw_frames > max_height)] = 0
-            raw_frames[raw_frames < min_height] = 0
-            raw_frames[raw_frames > max_height] = max_height
-            raw_frames = raw_frames.astype(frame_dtype)
+            raw_frames[raw_frames < config_data['min_height']] = 0
+            raw_frames[raw_frames > config_data['max_height']] = config_data['max_height']
+            raw_frames = raw_frames.astype(config_data['frame_dtype'])
             raw_frames = apply_roi(raw_frames, roi)
 
             results = extract_chunk(raw_frames,
-                                    use_em_tracker=use_tracking_model,
+                                    use_em_tracker=config_data['use_tracking_model'],
                                     strel_tail=strel_tail,
                                     strel_min=strel_min,
-                                    iters_tail=tail_filter_iters,
-                                    iters_min=cable_filter_iters,
-                                    prefilter_space=spatial_filter_size,
-                                    prefilter_time=temporal_filter_size,
-                                    min_height=min_height,
-                                    max_height=max_height,
-                                    flip_classifier=flip_classifier,
-                                    flip_smoothing=flip_classifier_smoothing,
-                                    crop_size=crop_size,
-                                    frame_dtype=frame_dtype,
-                                    mask_threshold=tracking_model_mask_threshold,
-                                    tracking_ll_threshold=tracking_model_ll_threshold,
-                                    tracking_segment=tracking_model_segment,
+                                    iters_tail=config_data['tail_filter_iters'],
+                                    iters_min=config_data['cable_filter_iters'],
+                                    prefilter_space=config_data['spatial_filter_size'],
+                                    prefilter_time=config_data['temporal_filter_size'],
+                                    min_height=config_data['min_height'],
+                                    max_height=config_data['max_height'],
+                                    flip_classifier=config_data['flip_classifier'],
+                                    flip_smoothing=config_data['flip_classifier_smoothing'],
+                                    crop_size=config_data['crop_size'],
+                                    frame_dtype=config_data['frame_dtype'],
+                                    mask_threshold=config_data['tracking_model_mask_threshold'],
+                                    tracking_ll_threshold=config_data['tracking_model_ll_threshold'],
+                                    tracking_segment=config_data['tracking_model_segment'],
                                     tracking_init_mean=tracking_init_mean,
                                     tracking_init_cov=tracking_init_cov,
                                     true_depth=true_depth,
-                                    centroid_hampel_span=centroid_hampel_span,
-                                    centroid_hampel_sig=centroid_hampel_sig,
-                                    angle_hampel_span=angle_hampel_span,
-                                    angle_hampel_sig=angle_hampel_sig,
-                                    model_smoothing_clips=model_smoothing_clips,
-                                    tracking_model_init=tracking_model_init)
+                                    centroid_hampel_span=config_data['centroid_hampel_span'],
+                                    centroid_hampel_sig=config_data['centroid_hampel_sig'],
+                                    angle_hampel_span=config_data['angle_hampel_span'],
+                                    angle_hampel_sig=config_data['angle_hampel_sig'],
+                                    model_smoothing_clips=config_data['model_smoothing_clips'],
+                                    tracking_model_init=config_data['tracking_model_init'])
 
             # if desired, write out a movie
 
             if i > 0:
-                offset = chunk_overlap
+                offset = config_data['chunk_overlap']
             else:
                 offset = 0
 
-            if use_tracking_model:
-                results['mask_frames'][results['depth_frames'] < min_height] = tracking_model_ll_clip
-                results['mask_frames'][results['mask_frames'] < tracking_model_ll_clip] = tracking_model_ll_clip
-                tracking_init_mean = results['parameters']['mean'][-(chunk_overlap+1)]
-                tracking_init_cov = results['parameters']['cov'][-(chunk_overlap+1)]
+            if config_data['use_tracking_model']:
+                results['mask_frames'][results['depth_frames'] < config_data['min_height']] = config_data['tracking_model_ll_clip']
+                results['mask_frames'][results['mask_frames'] < config_data['tracking_model_ll_clip']] = config_data['tracking_model_ll_clip']
+                tracking_init_mean = results['parameters']['mean'][-(config_data['chunk_overlap']+1)]
+                tracking_init_cov = results['parameters']['cov'][-(config_data['chunk_overlap']+1)]
 
             frame_range = frame_range[offset:]
 
@@ -448,19 +483,19 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
             f['frames'][frame_range] = results['depth_frames'][offset:, ...]
             f['frames_mask'][frame_range] = results['mask_frames'][offset:, ...]
 
-            if flip_classifier:
+            if config_data['flip_classifier']:
                 f['metadata/extraction/flips'][frame_range] = results['flips'][offset:]
 
             nframes, rows, cols = raw_frames[offset:, ...].shape
-            output_movie = np.zeros((nframes, rows+crop_size[0], cols+crop_size[1]), 'uint16')
-            output_movie[:, :crop_size[0], :crop_size[1]] = results['depth_frames'][offset:, ...]
-            output_movie[:, crop_size[0]:, crop_size[1]:] = raw_frames[offset:, ...]
+            output_movie = np.zeros((nframes, rows+config_data['crop_size'][0], cols+config_data['crop_size'][1]), 'uint16')
+            output_movie[:, :config_data['crop_size'][0], :config_data['crop_size'][1]] = results['depth_frames'][offset:, ...]
+            output_movie[:, config_data['crop_size'][0]:, config_data['crop_size'][1]:] = raw_frames[offset:, ...]
 
             video_pipe = write_frames_preview(
                 os.path.join(output_dir, '{}.mp4'.format(output_filename)), output_movie,
-                pipe=video_pipe, close_pipe=False, fps=fps,
+                pipe=video_pipe, close_pipe=False, fps=config_data['fps'],
                 frame_range=[f + first_frame_idx for f in frame_range],
-                depth_max=max_height, depth_min=min_height)
+                depth_max=config_data['max_height'], depth_min=config_data['min_height'])
 
         if video_pipe:
             video_pipe.stdin.close()
@@ -469,12 +504,12 @@ def extract_command(input_file, crop_size, bg_roi_dilate, bg_roi_shape, bg_roi_i
     print('\n')
 
     try:
-        if input_file.endswith('dat') and compress:
+        if input_file.endswith('dat') and config_data['compress']:
             convert_raw_to_avi_function(input_file,
-                                        chunk_size=compress_chunk_size,
-                                        fps=fps,
+                                        chunk_size=config_data['compress_chunk_size'],
+                                        fps=config_data['fps'],
                                         delete=False, # to be changed when we're ready!
-                                        threads=compress_threads)
+                                        threads=config_data['compress_threads'])
     except AttributeError as e:
         pass
 
