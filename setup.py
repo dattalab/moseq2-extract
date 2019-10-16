29267776a1ee04596542c36e15f5ab597c915502
Ayman Zeine <a.zeine.96@gmail.com>
2019-10-16 10:54:48 -0400

    Added setup dependency for travisCI

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     platforms=['mac', 'unix'],
     packages=find_packages(),
     install_requires=['h5py', 'matplotlib', 'scipy>=0.19,<1.3.0',
-                      'scikit-image>=0.13',
+                      'scikit-image>=0.13', 'cytoolz',
                       'tqdm', 'statsmodels>=0.8.0', 'numpy',
                       'joblib==0.13.1', 'scikit-learn',
                       'click', 'ruamel.yaml>=0.15.0'],
