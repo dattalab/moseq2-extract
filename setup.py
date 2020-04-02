fae9ae7626c3c238c190449d1d2ba53fccfc6ebc
Ayman Zeine <a.zeine.96@gmail.com>
2020-04-02 18:30:30 -0400

    bugfix: travis fix

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='moseq2-extract',
     author='Jeff Markowitz',
     description='To boldly go where no mouse has gone before',
-    version='0.3.0',
+    version='0.4.0',
     platforms=['mac', 'unix'],
     packages=find_packages(),
     install_requires=['h5py', 'matplotlib', 'scipy',
