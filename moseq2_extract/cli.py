3461d4ab7ce6e1a30e5921f59a9c91557e22e96b
Winthrop Gillis <win.gillis@gmail.com>
2020-11-15 21:39:01 -0500

    added batch extract

--- moseq2_extract/cli.py
+++ moseq2_extract/cli.py
@@ -9,7 +9,8 @@ Note: These functions simply read all the parameters into a dictionary,
 import os
 import click
 import ruamel.yaml as yaml
-from moseq2_extract.util import command_with_config, read_yaml
+from tqdm.auto import tqdm
+from moseq2_extract.util import command_with_config, read_yaml, recursive_find_unextracted_dirs
 from moseq2_extract.helpers.wrappers import (get_roi_wrapper, extract_wrapper, flip_file_wrapper,
                                              generate_index_wrapper, aggregate_extract_results_wrapper,
                                              convert_raw_to_avi_wrapper, copy_slice_wrapper)
@@ -77,7 +78,7 @@ def common_roi_options(function):
                             help='Number of erosion iterations to decrease bucket floor size. (Special Cases Only)')(function)
     function = click.option('--noise-tolerance', default=30, type=int,
                             help='Extent of noise to accept during RANSAC Plane ROI computation. (Special Cases Only)')(function)
-    function = click.option('--output-dir', default=None, help='Output directory to save the results h5 file')(function)
+    function = click.option('--output-dir', default='proc', help='Output directory to save the results h5 file')(function)
     function = click.option('--use-plane-bground', is_flag=True,
                             help='Use a plane fit for the background. Useful for mice that don\'t move much')(function)
     function = click.option("--config-file", type=click.Path())(function)
@@ -105,6 +106,49 @@ def common_avi_options(function):
     function = click.option('-t', '--threads', type=int, default=3, help='Number of threads for encoding')(function)
     return function
 
+def extract_options(function):
+    function = click.option('--crop-size', '-c', default=(80, 80), type=(int, int), help='Width and height of cropped mouse image')(function)
+    function = click.option('--num-frames', '-n', default=None, type=int, help='Number of frames to extract. Will extract full session if set to None.')(function)
+    function = click.option('--min-height', default=10, type=int, help='Min mouse height from floor (mm)')(function)
+    function = click.option('--max-height', default=100, type=int, help='Max mouse height from floor (mm)')(function)
+    function = click.option('--detected-true-depth', default='auto', type=str, help='Option to override automatic depth estimation during extraction. \
+This is only a debugging parameter, for cases where dilate_iterations > 1, otherwise has no effect. Either "auto" or an int value.')(function)
+    function = click.option('--compute-raw-scalars', is_flag=True, help="Compute scalar values from raw cropped frames.")(function)
+    function = click.option('--flip-classifier', default=None, help='Location of the flip classifier used to properly orient the mouse (.pkl file)')(function)
+    function = click.option('--flip-classifier-smoothing', default=51, type=int, help='Number of frames to smooth flip classifier probabilities')(function)
+    function = click.option('--graduate-walls', default=True, type=bool, help="Graduates and dilates the background image to compensate for slanted bucket walls. \\_/")(function)
+    function = click.option('--widen-radius', default=0, type=int, help="Number of pixels to increase/decrease radius by when graduating bucket walls.")(function)
+    function = click.option('--use-cc', default=True, type=bool, help="Extract features using largest connected components.")(function)
+    function = click.option('--use-tracking-model', default=False, type=bool, help='Use an expectation-maximization style model to aid mouse tracking. Useful for data with cables')(function)
+    function = click.option('--tracking-model-ll-threshold', default=-100, type=float, help="Threshold on log-likelihood for pixels to use for update during tracking")(function)
+    function = click.option('--tracking-model-mask-threshold', default=-16, type=float, help="Threshold on log-likelihood to include pixels for centroid and angle calculation")(function)
+    function = click.option('--tracking-model-ll-clip', default=-100, type=float, help="Clip log-likelihoods below this value")(function)
+    function = click.option('--tracking-model-segment', default=True, type=bool, help="Segment likelihood mask from tracking model")(function)
+    function = click.option('--tracking-model-init', default='raw', type=str, help="Method for tracking model initialization")(function)
+    function = click.option('--cable-filter-iters', default=0, type=int, help="Number of cable filter iterations")(function)
+    function = click.option('--cable-filter-shape', default='rectangle', type=str, help="Cable filter shape (rectangle or ellipse)")(function)
+    function = click.option('--cable-filter-size', default=(5, 5), type=(int, int), help="Cable filter size (in pixels)")(function)
+    function = click.option('--tail-filter-iters', default=1, type=int, help="Number of tail filter iterations")(function)
+    function = click.option('--tail-filter-size', default=(9, 9), type=(int, int), help='Tail filter size')(function)
+    function = click.option('--tail-filter-shape', default='ellipse', type=str, help='Tail filter shape')(function)
+    function = click.option('--spatial-filter-size', '-s', default=[3], type=int, help='Space prefilter kernel (median filter, must be odd)', multiple=True)(function)
+    function = click.option('--temporal-filter-size', '-t', default=[0], type=int, help='Time prefilter kernel (median filter, must be odd)', multiple=True)(function)
+    function = click.option('--chunk-overlap', default=0, type=int, help='Frames overlapped in each chunk. Useful for cable tracking')(function)
+    function = click.option('--write-movie', default=True, type=bool, help='Write a results output movie including an extracted mouse')(function)
+    function = click.option('--frame-dtype', default='uint8', type=click.Choice(['uint8', 'uint16']), help='Data type for processed frames')(function)
+    function = click.option('--centroid-hampel-span', default=0, type=int, help='Hampel filter span')(function)
+    function = click.option('--centroid-hampel-sig', default=3, type=float, help='Hampel filter sig')(function)
+    function = click.option('--angle-hampel-span', default=0, type=int, help='Angle filter span')(function)
+    function = click.option('--angle-hampel-sig', default=3, type=float, help='Angle filter sig')(function)
+    function = click.option('--model-smoothing-clips', default=(0, 0), type=(float, float), help='Model smoothing clips')(function)
+    function = click.option('--frame-trim', default=(0, 0), type=(int, int), help='Frames to trim from beginning and end of data')(function)
+    function = click.option('--compress', default=False, type=bool, help='Convert .dat to .avi after successful extraction')(function)
+    function = click.option('--compress-chunk-size', type=int, default=3000, help='Chunk size for .avi compression')(function)
+    function = click.option('--compress-threads', type=int, default=3, help='Number of threads for encoding')(function)
+    function = click.option('--skip-completed', is_flag=True, help='Will skip the extraction if it is already completed.')(function)
+
+    return function
+
 @cli.command(name="find-roi", cls=command_with_config('config_file'), help="Finds the ROI and background distance to subtract from frames when extracting.")
 @click.argument('input-file', type=click.Path(exists=True))
 @common_roi_options
@@ -118,48 +162,34 @@ def find_roi(input_file, output_dir, **config_data):
 @click.argument('input-file', type=click.Path(exists=True, resolve_path=True))
 @common_roi_options
 @common_avi_options
-@click.option('--crop-size', '-c', default=(80, 80), type=(int, int), help='Width and height of cropped mouse image')
-@click.option('--num-frames', '-n', default=None, type=int, help='Number of frames to extract. Will extract full session if set to None.')
-@click.option('--min-height', default=10, type=int, help='Min mouse height from floor (mm)')
-@click.option('--max-height', default=100, type=int, help='Max mouse height from floor (mm)')
-@click.option('--detected-true-depth', default='auto', type=str, help='Option to override automatic depth estimation during extraction. \
-            This is only a debugging parameter, for cases where dilate_iterations > 1, otherwise has no effect. Either "auto" or an int value.')
-@click.option('--compute-raw-scalars', is_flag=True, help="Compute scalar values from raw cropped frames.")
-@click.option('--flip-classifier', default=None, help='Location of the flip classifier used to properly orient the mouse (.pkl file)')
-@click.option('--flip-classifier-smoothing', default=51, type=int, help='Number of frames to smooth flip classifier probabilities')
-@click.option('--graduate-walls', default=True, type=bool, help="Graduates and dilates the background image to compensate for slanted bucket walls. \\_/")
-@click.option('--widen-radius', default=0, type=int, help="Number of pixels to increase/decrease radius by when graduating bucket walls.")
-@click.option('--use-cc', default=True, type=bool, help="Extract features using largest connected components.")
-@click.option('--use-tracking-model', default=False, type=bool, help='Use an expectation-maximization style model to aid mouse tracking. Useful for data with cables')
-@click.option('--tracking-model-ll-threshold', default=-100, type=float, help="Threshold on log-likelihood for pixels to use for update during tracking")
-@click.option('--tracking-model-mask-threshold', default=-16, type=float, help="Threshold on log-likelihood to include pixels for centroid and angle calculation")
-@click.option('--tracking-model-ll-clip', default=-100, type=float, help="Clip log-likelihoods below this value")
-@click.option('--tracking-model-segment', default=True, type=bool, help="Segment likelihood mask from tracking model")
-@click.option('--tracking-model-init', default='raw', type=str, help="Method for tracking model initialization")
-@click.option('--cable-filter-iters', default=0, type=int, help="Number of cable filter iterations")
-@click.option('--cable-filter-shape', default='rectangle', type=str, help="Cable filter shape (rectangle or ellipse)")
-@click.option('--cable-filter-size', default=(5, 5), type=(int, int), help="Cable filter size (in pixels)")
-@click.option('--tail-filter-iters', default=1, type=int, help="Number of tail filter iterations")
-@click.option('--tail-filter-size', default=(9, 9), type=(int, int), help='Tail filter size')
-@click.option('--tail-filter-shape', default='ellipse', type=str, help='Tail filter shape')
-@click.option('--spatial-filter-size', '-s', default=[3], type=int, help='Space prefilter kernel (median filter, must be odd)', multiple=True)
-@click.option('--temporal-filter-size', '-t', default=[0], type=int, help='Time prefilter kernel (median filter, must be odd)', multiple=True)
-@click.option('--chunk-overlap', default=0, type=int, help='Frames overlapped in each chunk. Useful for cable tracking')
-@click.option('--write-movie', default=True, type=bool, help='Write a results output movie including an extracted mouse')
-@click.option('--frame-dtype', default='uint8', type=click.Choice(['uint8', 'uint16']), help='Data type for processed frames')
-@click.option('--centroid-hampel-span', default=0, type=int, help='Hampel filter span')
-@click.option('--centroid-hampel-sig', default=3, type=float, help='Hampel filter sig')
-@click.option('--angle-hampel-span', default=0, type=int, help='Angle filter span')
-@click.option('--angle-hampel-sig', default=3, type=float, help='Angle filter sig')
-@click.option('--model-smoothing-clips', default=(0, 0), type=(float, float), help='Model smoothing clips')
-@click.option('--frame-trim', default=(0, 0), type=(int, int), help='Frames to trim from beginning and end of data')
-@click.option('--compress', default=False, type=bool, help='Convert .dat to .avi after successful extraction')
-@click.option('--compress-chunk-size', type=int, default=3000, help='Chunk size for .avi compression')
-@click.option('--compress-threads', type=int, default=3, help='Number of threads for encoding')
-@click.option('--skip-completed', is_flag=True, help='Will skip the extraction if it is already completed.')
+@extract_options
 def extract(input_file, output_dir, num_frames, skip_completed, **config_data):
 
     extract_wrapper(input_file, output_dir, config_data, num_frames=num_frames, skip=skip_completed)
+
+
+@cli.command(name='batch-extract', cls=command_with_config('config_file'), help='Batch processes '
+             'all the raw depth recordings located in the input folder.')
+@click.argument('input-folder', type=click.Path(exists=True, resolve_path=True))
+@common_roi_options
+@common_avi_options
+@extract_options
+@click.option('--extensions', default=['.dat'], type=str, help='File extension of raw data', multiple=True)
+@click.option('--skip-checks', is_flag=True, help='Flag: skip checks for the existance of a metadata file')
+def batch_extract(input_folder, output_dir, skip_completed, num_frames, extensions,
+                  skip_checks, **config_data):
+
+    to_extract = []
+    for ex in extensions:
+        to_extract.extend(
+            recursive_find_unextracted_dirs(input_folder, extension=ex,
+                skip_checks=True if ex in ('.tgz', '.tar.gz') else skip_checks,
+                 yaml_path=os.path.join(output_dir, 'results_00.yaml')))
+    for session in tqdm(to_extract):
+        extract_wrapper(session, output_dir, config_data, num_frames=num_frames,
+                        skip=skip_completed)
+
+    
 
 @cli.command(name="download-flip-file", help="Downloads Flip-correction model that helps with orienting the mouse during extraction.")
 @click.argument('config-file', type=click.Path(exists=True, resolve_path=True), default='config.yaml')
@@ -211,7 +241,7 @@ def convert_raw_to_avi(input_file, output_file, chunk_size, fps, delete, threads
 @cli.command(name="copy-slice", help='Copies a segment of an input depth recording into a new video file.')
 @click.argument('input-file', type=click.Path(exists=True, resolve_path=True))
 @common_avi_options
-@click.option('-c', '--copy-slice', type=(int, int), default=(0, 1000), help='Slice to copy')
+@click.option('-c', '--copy-slice', type=(int, int), default=(0, 1000), help='Slice indices used for copy')
 def copy_slice(input_file, output_file, copy_slice, chunk_size, fps, delete, threads):
 
     copy_slice_wrapper(input_file, output_file, copy_slice, chunk_size, fps, delete, threads)
