c6d37c09455847ed4a4620a0d91d98b5dc08e028
Winthrop Gillis <win.gillis@gmail.com>
2020-11-24 16:54:57 -0500

    roi dilation is not a special case

--- moseq2_extract/cli.py
+++ moseq2_extract/cli.py
@@ -72,7 +72,7 @@ def common_roi_options(function):
     function = click.option('--bg-sort-roi-by-position-max-rois', default=2, type=int,
                             help='Max original ROIs to sort by position')(function)
     function = click.option('--dilate-iterations', default=1, type=int,
-                            help='Number of dilation iterations to increase bucket floor size. (Special Cases Only)')(function)
+                            help='Number of dilation iterations to increase bucket floor size.')(function)
     function = click.option('--bg-roi-erode', default=(1, 1), type=(int, int),
                             help='Size of cv2 Structure Element to erode roi. (Special Cases Only)')(function)
     function = click.option('--erode-iterations', default=0, type=int,
