3a2233da91b2fde8a0ccafe2f51a4bad9e682645
Sherry <sherrylin42@gmail.com>
2021-12-28 15:55:08 -0500

    chore: true_depth not cast to int

--- moseq2_extract/helpers/wrappers.py
+++ moseq2_extract/helpers/wrappers.py
@@ -397,7 +397,7 @@ def extract_wrapper(input_file, output_dir, config_data, num_frames=None, skip=F
 
     status_dict['complete'] = True
     if status_dict['parameters'].get('true_depth') is None:
-        status_dict['parameters']['true_depth'] = int(config_data.get('true_depth'))
+        status_dict['parameters']['true_depth'] = config_data.get('true_depth')
 
     with open(status_filename, 'w') as f:
         yaml.safe_dump(status_dict, f)
